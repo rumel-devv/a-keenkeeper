bba8f33b7b53a8a73dc09d27447626d9c2ea599a
MD RUMEL AHMED <rumelrumel0987654321@gmail.com>
2026-04-13 21:08:37 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, quas. </h1>
+
+  </StrictMode>,
+)
