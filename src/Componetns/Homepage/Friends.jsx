f9121e6ea9bbffd375b58638b2c63bfd761f36f5
MD RUMEL AHMED <rumelrumel0987654321@gmail.com>
2026-04-14 08:30:29 +0600

    added friends card section

--- src/Componetns/Homepage/Friends.jsx
+++ src/Componetns/Homepage/Friends.jsx
@@ -1,19 +1,17 @@
 import React, { use } from 'react';
+import FriendsCard from './FriendsCard';
 
 const friendPromise = fetch('/data.json').then(res => res.json()) 
 
 const Friends = () => {
 
     const friends = use(friendPromise)
-    console.log(friends);
 
     return (
         <div className='my-6 space-y-4'>
             <h2 className='text-xl'>Your friends</h2>
-            <div>
-             {friends.map(friend => <div> 
-                       <h2> {friend.name} </h2>
-                 </div>)}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+             {friends.map(friend => <FriendsCard key={friend.id} friend={friend} ></FriendsCard>)}
             </div>
         </div>
     );
---
+++ src/Componetns/Homepage/FriendsCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const FriendsCard = ({friend}) => {
+  console.log(friend);
+    return (
+        <div>
+            <div className='bg-[#f1ebeb74] text-center space-y-2 py-6'>
+                <img className='w-20 h-20 rounded-full mx-auto' src={friend.picture} alt={friend.name}/>
+                <h2 className='text-xl font-semibold'> {friend.name} </h2>
+                <h3> {friend.days_since_contact}d ago </h3>
+                 <h3 className="badge badge-soft badge-success text-black "> {friend.tags} </h3>
+                 <br />
+                 <div  className={`badge ${
+                     friend.status === "ontrack"
+                      ? "bg-[#244D3F] text-white"
+                     : friend.status === "overdue"
+                     ? "bg-red-500 text-white"
+                     : friend.status === "almost_due"
+                     ? "bg-yellow-500 text-white"
+                     : ""
+                     }`} > {friend.status} </div>
+            </div>
+        </div>
+    );
+};
+
+export default FriendsCard;
